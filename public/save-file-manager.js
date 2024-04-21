c9269ff3fc52e8f779bda367dee9889635079f6d
Booster2ooo <the._.truth@hotmail.fr>
2024-04-21 16:04:46 +0200

    Update save-file-manager.js

--- public/save-file-manager.js
+++ public/save-file-manager.js
@@ -35,47 +35,51 @@ Storage.prototype.clear = new Proxy(Storage.prototype.clear, {
   },
 });
 
+let savingEnabled = true;
 const saveFileKey = 'goobooSavefile';
-const getSaveFile = async () => fetch('/save')
+const saveApiEndpoint = `${applicationPath}/save`;
+const getSaveFile = () => fetch(saveApiEndpoint)
   .then(res => res.text())
   .then(save => {
     if (save) {
       const currentSave = localStorage.getItem(saveFileKey);
       if (save !== currentSave) {
+        savingEnabled = false;
         localStorage.setItem(saveFileKey, save);
         window.location.reload();
       }
     }
   })
   .catch(err => console.error(err));
-
 const updateSaveFile = async () => fetch(
-    '/save', 
-    {
-      method: 'POST',
-      body: localStorage.getItem(saveFileKey)
-    }
-  )
-  .catch(err => console.error(err));
+  saveApiEndpoint, 
+  {
+    method: 'POST',
+    body: localStorage.getItem(saveFileKey)
+  }
+)
+.catch(err => console.error(err));
 
 
-window.onload = async () => await getSaveFile;
+window.onload = getSaveFile;
 
 window.onstorage = async ({ key, newValue, oldValue, storageArea }) => {
-  if (key === saveFileKey && newValue !== oldValue) {
+  if (savingEnabled && key === saveFileKey && newValue !== oldValue) {
     await updateSaveFile();
   }
 };
 
 window.onbeforeunload = async () => {
-  const vue = document.getElementById('app').__vue__;
-  const header = vue.$children.find(c => c.$vnode.tag.includes('v-app-bar'));
-  const menu = header.$children.filter(c => c.$vnode.tag.includes('v-menu'))[1];
-  const toggleBtn = menu.$children[0];
-  toggleBtn.$el.dispatchEvent(new Event('mouseenter'));
-  setTimeout(() => {	
-	  const saveBtn = menu.$children[1].$children[0].$children[0].$el;
-	  saveBtn.click();
-  }, 100);
-  await updateSaveFile();
+  if (savingEnabled) {
+    const vue = document.getElementById('app').__vue__;
+    const header = vue.$children.find(c => c.$vnode.tag.includes('v-app-bar'));
+    const menu = header.$children.filter(c => c.$vnode.tag.includes('v-menu'))[1];
+    const toggleBtn = menu.$children[0];
+    toggleBtn.$el.dispatchEvent(new Event('mouseenter'));
+    setTimeout(async () => {	
+      const saveBtn = menu.$children[1].$children[0].$children[0].$el;
+      saveBtn.click();
+      await updateSaveFile();
+    }, 100);
+  }
 };
